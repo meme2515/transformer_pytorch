17ff213040ebcc18307329d6542f5c03341bb51a
meme2515 <meme2515@gmail.com>
2023-05-04 13:55:22 +0900

    code modularization

--- train.py
+++ train.py
@@ -1,8 +1,5 @@
-# QUAK Dataset : https://www.aihub.or.kr/aihubdata/data/view.do?currMenu=120&topMenu=100&aihubDataSe=extrldata&dataSetSn=71268
-
 from model.model import Transformer
 from torch.utils.data import DataLoader
-from torch.optim.lr_scheduler import StepLR
 from torchtext.datasets import Multi30k
 from util import tokenizer
 
@@ -11,10 +8,7 @@ import torch
 import torch.nn as nn
 import time
 import wandb
-import os
 import argparse
-import shutil
-import sys
 
 # DEVICE = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -23,52 +17,44 @@ D_MODEL = 512
 MAX_LEN = 256
 N_HEADS = 8
 N = 6
-SRC_VOCAB = tokenizer.de_vocab()
-TGT_VOCAB = tokenizer.en_vocab()
-LEARNING_RATE = 0.1
+LEARNING_RATE = 0.0001
 BETA1, BETA2 = 0.9, 0.98
-
+WARMUP = 30
+CLIP = 1
 SRC_LANGUAGE = 'de'
 TGT_LANGUAGE = 'en'
 
-de_dict = tokenizer.de_dict()
-en_dict = tokenizer.en_dict()
+# Count Parameters
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+# Weight Initializer
+def initialize_weights(m):
+    if hasattr(m, 'weight') and m.weight.dim() > 1:
+        nn.init.kaiming_uniform_(m.weight.data)
+
+# Current Learning Rate
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+    
+tokenizer = tokenizer.Tokenizer(SRC_LANGUAGE, TGT_LANGUAGE)
+src_vocab = tokenizer.src_vocab()
+tgt_vocab = tokenizer.tgt_vocab()
 
 wandb.init(
     project="transformer v1",
-
-    config={
-        "learning_rate" : LEARNING_RATE,
-        "architecture" : "Transformer",
-        "dataset" : "Multi30k",
-        "beta1" : BETA1,
-        "beta2" : BETA2
-    }
 )
 
+# Transformer
 model = Transformer(
-    src_vocab=SRC_VOCAB,
-    tgt_vocab=TGT_VOCAB,
+    src_vocab=len(src_vocab),
+    tgt_vocab=len(tgt_vocab),
     d_model=D_MODEL,
     max_len=MAX_LEN,
     n_heads=N_HEADS,
     N=N,
-    # device=DEVICE
 )
-
-# MPS & CUDA
-model.to(DEVICE)
-wandb.watch(model, log=None)
-
-def count_parameters(model):
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
-
-def initialize_weights(m):
-    if hasattr(m, 'weight') and m.weight.dim() > 1:
-        nn.init.kaiming_uniform(m.weight.data)
-
-print(f'The model has {count_parameters(model):,} trainable parameters')
-model.apply(initialize_weights)
 
 # Adam Optimizer
 optimizer = torch.optim.Adam(
@@ -82,126 +68,105 @@ optimizer = torch.optim.Adam(
 # LR Scheduler
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer, 
                                                        verbose=True, 
-                                                       factor=0.9, 
-                                                       patience=10)
+                                                       factor=0.5, 
+                                                       patience=3)
 
 # Cross Entropy Loss
-criterion = nn.CrossEntropyLoss(ignore_index=1)
+criterion = nn.CrossEntropyLoss()
 
-def get_lr(optimizer):
-    for param_group in optimizer.param_groups:
-        return param_group['lr']
+# MPS & CUDA
+model.to(DEVICE)
+model.apply(initialize_weights)
 
-def idx_to_word(x, vocab):
-    words = []
-    x = x[:30]
-    for i in x:
-        word = vocab.get_itos()[i]
-        if '<' not in word:
-            words.append(word)
-    words = " ".join(words)
-    return words
+wandb.watch(model, log=None)
 
 # Training Step
-def train(epochs, print_every=100):
+def train(model, criterion, dataloader, optimizer, clip):
     model.train()
-    
-    start = time.time()
-    temp = start
-    train_loss = 0
-    val_loss = 0
-    epoch_train_loss = 0
-    epoch_val_loss = 0
-    train_steps = 0
-    val_steps = 0
+    train_loss, train_steps = 0, 0
 
-    train_iter = Multi30k(split='train', language_pair=(SRC_LANGUAGE, TGT_LANGUAGE))
-    train_dataloader = DataLoader(train_iter, batch_size=128, collate_fn=tokenizer.collate_fn, shuffle=True)
+    # Train Step
+    for src, tgt in dataloader:
+        source = src.transpose(-2, -1).to(DEVICE)
+        target = tgt.transpose(-2, -1).to(DEVICE)
 
-    val_iter = Multi30k(split='valid', language_pair=(SRC_LANGUAGE, TGT_LANGUAGE))
-    val_dataloader = DataLoader(val_iter, batch_size=128, collate_fn=tokenizer.collate_fn, shuffle=True)
-    
-    # model.train(True)
-    for epoch in range(epochs):
-        i = 0
-        # Train Step
-        for src, tgt in train_dataloader:
+        target_input = target[:, :-1]
+        target_output = target[:, 1:].contiguous().view(-1)
+
+        optimizer.zero_grad()
+        preds = model(source, target_input)
+            
+        loss = criterion(preds.view(-1, preds.size(-1)), target_output)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+        optimizer.step()
+        wandb.log({"step train loss": loss})
+
+        train_loss += loss.data
+        train_steps += 1
+
+    return train_loss / (train_steps + 1)
+
+# Evaluation Step
+def evaluate(model, criterion, dataloader, tokenizer, vocab):
+    model.eval()
+    val_loss, val_steps = 0, 0
+
+    with torch.no_grad():
+        for src, tgt in dataloader:
             source = src.transpose(-2, -1).to(DEVICE)
             target = tgt.transpose(-2, -1).to(DEVICE)
 
             target_input = target[:, :-1]
             target_output = target[:, 1:].contiguous().view(-1)
 
-            optimizer.zero_grad()
             preds = model(source, target_input)
-            
             loss = criterion(preds.view(-1, preds.size(-1)), target_output)
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
-            optimizer.step()
-            wandb.log({"step train loss": loss})
-
-            train_loss += loss.data
-            epoch_train_loss += loss.data
-
-            if (i + 1) % print_every == 0:
-                loss_avg = train_loss / print_every
-                print("Train : time = %dm, epoch %d, iter = %d, loss = %.10f, lr = %.10f, %ds per %d iters" % \
-                      ((time.time() - start) // 60, epoch + 1, i + 1, loss_avg, get_lr(optimizer), time.time() - temp, print_every))
-                train_loss = 0
-                temp = time.time()
-                
-            i += 1
-
-        train_steps = i + 1
-        if (epoch + 1) % 100 == 0:
-            torch.save(model, 'checkpoint_ep_%d.pt'%(epoch + 1))
-            
-        j = 0
-        # Validation Step
-        with torch.no_grad():
-            for src, tgt in val_dataloader:
-                source = src.transpose(-2, -1).to(DEVICE)
-                target = tgt.transpose(-2, -1).to(DEVICE)
-
-                target_input = target[:, :-1]
-                target_output = target[:, 1:].contiguous().view(-1)
-
-                preds = model(source, target_input)
-                loss = criterion(preds.view(-1, preds.size(-1)), target_output)
-
-                epoch_val_loss += loss.data
-
-                if (j + 1) % (print_every) == 0:
-                    loss_avg = val_loss / print_every
-                    print("Validation : time = %dm, epoch %d, iter = %d, loss = %.10f, lr = %.10f, %ds per %d iters" % \
-                          ((time.time() - start) // 60, epoch + 1, j + 1, loss_avg, get_lr(optimizer), time.time() - temp, print_every))
-                    val_loss = 0
-                    temp = time.time()
-                j += 1
-            val_steps = j + 1
+            val_loss += loss.data
+            val_steps += 1
 
         predicted_sentence = torch.argmax(preds.view(-1, preds.size(-1)), dim=1)
-        # print(predicted_sentence.shape)
-        predicted_sentence = idx_to_word(predicted_sentence, en_dict)
-        original_sentence = idx_to_word(target_output, en_dict)
+        predicted_sentence = tokenizer.idx_to_word(predicted_sentence, vocab)
+        original_sentence = tokenizer.idx_to_word(target_output, vocab)
         print("Predicted Sentence :", predicted_sentence)
         print("Original Sentence :", original_sentence)
+    
+    return val_loss / (val_steps + 1)
 
+# Train & Evaluation Run
+def run(epochs, best_loss):
+    print(f'The model has {count_parameters(model):,} trainable parameters')
+
+    train_iter = Multi30k(split='train', language_pair=(SRC_LANGUAGE, TGT_LANGUAGE))
+    train_dataloader = DataLoader(train_iter, batch_size=128, collate_fn=tokenizer.collate_fn, shuffle=True)
+    eval_iter = Multi30k(split='valid', language_pair=(SRC_LANGUAGE, TGT_LANGUAGE))
+    eval_dataloader = DataLoader(eval_iter, batch_size=128, collate_fn=tokenizer.collate_fn, shuffle=True)
+
+    for epoch in range(epochs):
+        start_time = time.time()
+        train_loss = train(model, criterion, train_dataloader, optimizer, CLIP)
+        eval_loss = evaluate(model, criterion, eval_dataloader, tokenizer, tgt_vocab)
+        end_time = time.time()
+
+        if epoch > WARMUP:
+            scheduler.step(eval_loss)
+
+        if eval_loss < best_loss:
+            best_loss = eval_loss
+            torch.save(model.state_dict(), 'model.pt')
+        
         wandb.log({
-            "epoch train loss": epoch_train_loss / train_steps,
-            "epoch validation loss": epoch_val_loss / val_steps,
+            "epoch train loss": train_loss,
+            "epoch validation loss": eval_loss,
         })
 
-        if epoch > 100:
-            scheduler.step(epoch_val_loss / val_steps)
-
-        epoch_train_loss = 0
-        epoch_val_loss = 0
+        print("Epoch %d : time = %dm, train_loss = %.10f, eval_loss = %.10f, lr = %.10f" % \
+              (epoch + 1, (end_time - start_time) // 60, train_loss, eval_loss, get_lr(optimizer)))
+        
 
 # System Call
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--epoch', type=int, required=True)
+    parser.add_argument('--epochs', type=int, required=True)
     args = parser.parse_args()
-    train(epochs=args.epoch)
+    run(epochs=args.epochs, best_loss=100)
--- util/tokenizer.py
+++ util/tokenizer.py
@@ -1,5 +1,10 @@
 # Source : https://tutorials.pytorch.kr/beginner/translation_transformer.html
 
+# pip install -U torchdata
+# pip install -U spacy
+# python -m spacy download en_core_web_sm
+# python -m spacy download de_core_news_sm
+
 from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
 from torchtext.datasets import multi30k, Multi30k
@@ -8,102 +13,99 @@ from torch.nn.utils.rnn import pad_sequence
 
 import torch
 
-
 # 원본 데이터의 링크가 동작하지 않으므로 데이터셋의 URL을 수정해야 합니다.
 # 더 자세한 내용은 https://github.com/pytorch/text/issues/1756#issuecomment-1163664163 을 참고해주세요.
 multi30k.URL["train"] = "https://raw.githubusercontent.com/neychev/small_DL_repo/master/datasets/Multi30k/training.tar.gz"
 multi30k.URL["valid"] = "https://raw.githubusercontent.com/neychev/small_DL_repo/master/datasets/Multi30k/validation.tar.gz"
 
-SRC_LANGUAGE = 'de'
-TGT_LANGUAGE = 'en'
+class Tokenizer:
+    def __init__(self, src_language, tgt_language):
+        self.src_language = src_language
+        self.tgt_language = tgt_language
+        self.token_transform = {}
+        self.vocab_transform = {}
 
-# Place-holders
-token_transform = {}
-vocab_transform = {}
+        # 출발어(source)와 목적어(target)의 토크나이저(tokenizer)를 생성합니다.
+        self.token_transform[self.src_language] = get_tokenizer('spacy', language='de_core_news_sm')
+        self.token_transform[self.tgt_language] = get_tokenizer('spacy', language='en_core_web_sm')
 
-
-# 출발어(source)와 목적어(target)의 토크나이저(tokenizer)를 생성합니다.
-# 아래 필요 사항(dependency)을 모두 설치해주세요.
-# pip install -U torchdata
-# pip install -U spacy
-# python -m spacy download en_core_web_sm
-# python -m spacy download de_core_news_sm
-token_transform[SRC_LANGUAGE] = get_tokenizer('spacy', language='de_core_news_sm')
-token_transform[TGT_LANGUAGE] = get_tokenizer('spacy', language='en_core_web_sm')
-
-
-# 토큰 목록을 생성하기 위한 헬퍼(helper) 함수
-def yield_tokens(data_iter: Iterable, language: str) -> List[str]:
-    language_index = {SRC_LANGUAGE: 0, TGT_LANGUAGE: 1}
-
-    for data_sample in data_iter:
-        yield token_transform[language](data_sample[language_index[language]])
+        # 특수 기호(symbol)와 인덱스를 정의합니다
+        self.UNK_IDX, self.PAD_IDX, self.BOS_IDX, self.EOS_IDX = 0, 1, 2, 3
+        # 토큰들이 어휘집(vocab)에 인덱스 순서대로 잘 삽입되어 있는지 확인합니다
+        self.special_symbols = ['<unk>', '<pad>', '<bos>', '<eos>']
+        self.tokenize()
 
 
-# 특수 기호(symbol)와 인덱스를 정의합니다
-UNK_IDX, PAD_IDX, BOS_IDX, EOS_IDX = 0, 1, 2, 3
-# 토큰들이 어휘집(vocab)에 인덱스 순서대로 잘 삽입되어 있는지 확인합니다
-special_symbols = ['<unk>', '<pad>', '<bos>', '<eos>']
+    # 토큰 목록을 생성하기 위한 헬퍼(helper) 함수
+    def yield_tokens(self, data_iter: Iterable, language: str) -> List[str]:
+        language_index = {self.src_language: 0, self.tgt_language: 1}
+        for data_sample in data_iter:
+            yield self.token_transform[language](data_sample[language_index[language]])
 
 
-for ln in [SRC_LANGUAGE, TGT_LANGUAGE]:
-    # 학습용 데이터 반복자(iterator)
-    train_iter = Multi30k(split='train', language_pair=(SRC_LANGUAGE, TGT_LANGUAGE))
-    # torchtext의 Vocab(어휘집) 객체 생성
-    vocab_transform[ln] = build_vocab_from_iterator(yield_tokens(train_iter, ln),
-                                                    min_freq=1,
-                                                    specials=special_symbols,
-                                                    special_first=True)
-
-# UNK_IDX를 기본 인덱스로 설정합니다. 이 인덱스는 토큰을 찾지 못하는 경우에 반환됩니다.
-# 만약 기본 인덱스를 설정하지 않으면 어휘집(Vocabulary)에서 토큰을 찾지 못하는 경우
-# RuntimeError가 발생합니다.
-for ln in [SRC_LANGUAGE, TGT_LANGUAGE]:
-    vocab_transform[ln].set_default_index(UNK_IDX)
+    # Numerizer Dictionary
+    def tokenize(self):
+        for ln in [self.src_language, self.tgt_language]:
+            # 학습용 데이터 반복자(iterator)
+            train_iter = Multi30k(split='train', language_pair=(self.src_language, self.tgt_language))
+            # torchtext의 Vocab(어휘집) 객체 생성
+            self.vocab_transform[ln] = build_vocab_from_iterator(self.yield_tokens(train_iter, ln),
+                                                                 min_freq=1,
+                                                                 specials=self.special_symbols,
+                                                                 special_first=True)
+            self.vocab_transform[ln].set_default_index(self.UNK_IDX)
 
 
-# 순차적인 작업들을 하나로 묶는 헬퍼 함수
-def sequential_transforms(*transforms):
-    def func(txt_input):
-        for transform in transforms:
-            txt_input = transform(txt_input)
-        return txt_input
-    return func
-
-# BOS/EOS를 추가하고 입력 순서(sequence) 인덱스에 대한 텐서를 생성하는 함수
-def tensor_transform(token_ids: List[int]):
-    return torch.cat((torch.tensor([BOS_IDX]),
-                      torch.tensor(token_ids),
-                      torch.tensor([EOS_IDX])))
-
-# 출발어(src)와 도착어(tgt) 원시 문자열들을 텐서 인덱스로 변환하는 변형(transform)
-text_transform = {}
-for ln in [SRC_LANGUAGE, TGT_LANGUAGE]:
-    text_transform[ln] = sequential_transforms(token_transform[ln], # 토큰화(Tokenization)
-                                               vocab_transform[ln], # 수치화(Numericalization)
-                                               tensor_transform) # BOS/EOS를 추가하고 텐서를 생성
+    # 순차적인 작업들을 하나로 묶는 헬퍼 함수
+    def sequential_transforms(self, *transforms):
+        def func(txt_input):
+            for transform in transforms:
+                txt_input = transform(txt_input)
+            return txt_input
+        return func
 
 
-# 데이터를 텐서로 조합(collate)하는 함수
-def collate_fn(batch):
-    src_batch, tgt_batch = [], []
-    for src_sample, tgt_sample in batch:
-        src_batch.append(text_transform[SRC_LANGUAGE](src_sample.rstrip("\n")))
-        tgt_batch.append(text_transform[TGT_LANGUAGE](tgt_sample.rstrip("\n")))
-
-    src_batch = pad_sequence(src_batch, padding_value=PAD_IDX)
-    tgt_batch = pad_sequence(tgt_batch, padding_value=PAD_IDX)
-    return src_batch, tgt_batch
+    # BOS/EOS를 추가하고 입력 순서(sequence) 인덱스에 대한 텐서를 생성하는 함수
+    def tensor_transform(self, token_ids: List[int]):
+        return torch.cat((torch.tensor([self.BOS_IDX]),
+                          torch.tensor(token_ids),
+                          torch.tensor([self.EOS_IDX])))
 
 
-def de_vocab():
-    return len(vocab_transform[SRC_LANGUAGE])
+    # 데이터를 텐서로 조합(collate)하는 함수
+    def collate_fn(self, batch):
+        # 출발어(src)와 도착어(tgt) 원시 문자열들을 텐서 인덱스로 변환하는 변형(transform)
+        text_transform = {}
+        for ln in [self.src_language, self.tgt_language]:
+            text_transform[ln] = self.sequential_transforms(self.token_transform[ln], # 토큰화(Tokenization)
+                                                            self.vocab_transform[ln], # 수치화(Numericalization)
+                                                            self.tensor_transform) # BOS/EOS를 추가하고 텐서를 생성
+        src_batch, tgt_batch = [], []
+        for src_sample, tgt_sample in batch:
+            src_batch.append(text_transform[self.src_language](src_sample.rstrip("\n")))
+            tgt_batch.append(text_transform[self.tgt_language](tgt_sample.rstrip("\n")))
 
-def en_vocab():
-    return len(vocab_transform[TGT_LANGUAGE])
+        src_batch = pad_sequence(src_batch, padding_value=self.PAD_IDX)
+        tgt_batch = pad_sequence(tgt_batch, padding_value=self.PAD_IDX)
+        return src_batch, tgt_batch
 
-def de_dict():
-    return vocab_transform[SRC_LANGUAGE]
 
-def en_dict():
-    return vocab_transform[TGT_LANGUAGE]
+    # Source Language Vocabulary
+    def src_vocab(self):
+        return self.vocab_transform[self.src_language]
+
+
+    # Target Language Vocabulary
+    def tgt_vocab(self):
+        return self.vocab_transform[self.tgt_language]
+    
+    # Convert Indices to words
+    def idx_to_word(self, idx, vocab):
+        words = []
+        idx = idx[:30]
+        for i in idx:
+            word = vocab.get_itos()[i]
+            if '<' not in word:
+                words.append(word)
+        words = " ".join(words)
+        return words
